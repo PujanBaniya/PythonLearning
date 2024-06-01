37e575736fa10f00bd41beb42aab878ca7d54e36
PujanBaniya <139758537+PujanBaniya@users.noreply.github.com>
2024-06-01 19:04:55 +0545

    Add files via upload
    This is simple cafe management system using python

---
+++ coffeeManagement.py
@@ -0,0 +1,44 @@
+import datetime
+class Cafe:
+
+    def __init__(self):
+        self.name= input("Please enter your name==>");
+    def great(self):
+        print(f"Hello {self.name}, Welcome to Python Cafe\n Make your day with Taste\n Python Cafe\n Remove stress \n best by TASTE")
+        print("What can Python cafe serve you? Feel free to choose")
+        
+    def order(self):
+        menu={
+         "Coffee":"150",
+         "Tea":"30",
+         "Cake":"800",
+         "Cup Cake":"35"
+        }
+        x=1
+        price=0
+        while(x==1):
+            menu={
+             "Coffee":"150",
+             "Tea":"30",
+             "Cake":"800",
+             "Cup Cake":"35"
+            }
+            print("Coffee = 150\n Tea=30\n Cake=800\n Cup Cake=35")
+            print(f"Your price till now is {price}")
+    
+
+            order1 = input("Enter your order ==>");
+            if order1 in menu:
+                price = price+ int(menu[order1])
+                temp= input("Do you want to add more orders? Y/N==> ")
+                if temp=='Y':
+                    x=1;
+                else:
+                    x=0; 
+            else:
+                print(f"Sorry {self.name}, We are unable to serve you the {order1}")
+        print(f"\nPython cafe is happy to serve {self.name}.\n {self.name}'s had order of Rs {price}\n ")
+
+user = Cafe()
+user.great()
+user.order()
